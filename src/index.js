fa1c52c7d2521d0ba63d73cc0ef3d94e08ea8dba
Keti Tsiskaridze <ketitsiskaridze@gmail.com>
2023-06-30 00:30:01 +0400

    task resolved

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@
  * @returns {Array}
  */
 module.exports.numberOfDuplicates = function numberOfDuplicates(arr) {
-    // Your implementation here
-    throw new Error('Task not implemented');
+    const counts = {};
+    const result = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i];
+      if (counts.hasOwnProperty(element)) {
+        counts[element]++;
+      } else {
+        counts[element] = 1;
+      }
+      result.push(counts[element]);
+    }
+  
+    return result;
 };
 
 /**
@@ -14,6 +26,40 @@ module.exports.numberOfDuplicates = function numberOfDuplicates(arr) {
  * @returns {Number}
  */
 module.exports.countObjectStrength = function countObjectStrength(obj) {
-    // Your implementation here
-    throw new Error('Task not implemented');
-};
+    let strength = 0;
+  
+    const properties = Object.getOwnPropertyNames(obj);
+  
+    for (const key of properties) {
+        switch (typeof obj[key]) {
+            case "undefined":
+                strength += 0;
+                break;
+            case "boolean":
+                strength += 1;
+                break;
+            case "number":
+                strength += 2;
+                break;
+            case "string":
+                strength += 3;
+                break;
+            case "object":
+                strength += 5;
+                break;
+            case "function":
+                strength += 7;
+                break;
+            case "bigint":
+                strength += 9;
+                break;
+            case "symbol":
+                strength += 10;
+                break;
+            default:
+                break;
+        }
+    }
+  
+    return strength;
+  };
